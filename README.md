f41edcf5f2c73fe79629459d3c7fd2ec59413db1
Atogui <atogui10@gmail.com>
2026-03-25 04:23:54 +0100

    Añadidas nuevas funcionalidades

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Aplicación de tareas que permite crear, completar, eliminar y filtrar tareas.
 
-#Cambio para practicar flujo de Git
+La aplicación tiene las siguientes secciones: Título, cabecera, filtros, lista de tareas, formulario para añadir una tarea y panel de estadísticas
+El usuario podrá añadir tareas, modificar tareas, eliminar las tareas, marcas las tareas como completadas, filtrar las tareas que se muestran y ver estadísticas
---
+++ app.js
@@ -0,0 +1,165 @@
+let tareas = [];
+let filtroActivo = "Todas";
+
+const form = document.getElementById("form_tarea");
+const input = document.getElementById("input_tarea");
+const lista = document.getElementById("Li_tareas");
+const template = document.getElementById("tarea_template");
+const stats = document.getElementById("stats");
+const totalEl = document.getElementById("total");
+const completedEl = document.getElementById("completadas");
+const pendingEl = document.getElementById("pendientes");
+const botones = document.querySelectorAll(".bot_filtro");
+const completeAll = document.getElementById("complete");
+const wipe = document.getElementById("wipe");
+const searchEl = document.getElementById("search");
+
+
+cargarTareas();
+renderTareas();
+
+function crearTarea(title){
+    return{
+        id: Date.now(),
+        title: title,
+        completed: false,
+        createdAt: new Date().toISOString()
+    };
+}
+
+botones.forEach(btn => {
+    btn.addEventListener("click", () => {
+        botones.forEach(b => b.classList.replace("bg-[rgb(34,_173,_46)]","bg-[rgb(51,_51,_51)]"));
+        btn.classList.replace("bg-[rgb(51,_51,_51)]","bg-[rgb(34,_173,_46)]");
+
+        filtroActivo = btn.textContent;
+        renderTareas();
+    });
+});
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  const texto = input.value.trim();
+  if (texto === "") return;
+
+  const nuevaTarea = crearTarea(input.value);
+
+  tareas.push(nuevaTarea);
+
+  renderTareas();
+  guardarTareas();
+
+  input.value = "";
+});
+
+completeAll.addEventListener("click", () => {
+    tareas.forEach(t => t.completed = true);
+    renderTareas();
+    guardarTareas();
+});
+
+wipe.addEventListener("click", () => {
+    tareas = tareas.filter(t => !t.completed);
+    renderTareas();
+    guardarTareas();
+});
+
+searchEl.addEventListener("keyup", () => {
+    renderTareas();
+});
+
+function renderTareas(){
+  lista.innerHTML = "";
+
+  tareasFiltradas = tareas;
+
+    if(searchEl.value){
+        tareasFiltradas = tareasFiltradas.filter(t => t.title.toLowerCase().includes(searchEl.value.toLowerCase()));
+    }
+
+    if(filtroActivo === "Completadas"){
+        tareasFiltradas = tareas.filter(t => t.completed);
+    }
+
+    if(filtroActivo === "Pendientes"){
+        tareasFiltradas = tareas.filter(t => !t.completed);
+    }
+
+
+
+  tareasFiltradas.forEach(tarea =>{
+
+    const clone = template.content.cloneNode(true);
+
+    const texto = clone.querySelector(".desc_texto");
+    texto.textContent = tarea.title;
+
+    clone.querySelector(".edit").addEventListener("click", () => {
+      
+      const input = document.createElement("input");
+      input.type = "text";
+      input.value = tarea.title;
+
+      texto.replaceWith(input);
+
+      input.focus();
+
+      input.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+          tarea.title = input.value;
+          renderTareas();
+        }
+      });
+
+      input.addEventListener("blur", () => {
+        tarea.title = input.value;
+        renderTareas();
+      });
+    });
+
+    clone.querySelector(".check").addEventListener("click", () =>{
+        tarea.completed = !tarea.completed;
+        renderTareas();
+        guardarTareas();
+    });
+
+   clone.querySelector(".delete").addEventListener("click", () =>{
+        tareas = tareas.filter(t => t.id !== tarea.id);
+        renderTareas();
+        guardarTareas();
+        renderStats();
+    });
+
+    if (tarea.completed){
+        clone.querySelector(".desc_texto").classList.add("line-through");
+    }
+
+    lista.appendChild(clone);
+    renderStats();
+  });
+}
+
+function renderStats(){
+    
+    const total = tareas.length;
+
+    const completed = tareas.filter(t => t.completed).length;
+
+    const pending = total - completed;
+
+    totalEl.textContent = total;
+    completedEl.textContent = completed;
+    pendingEl.textContent = pending;
+}
+
+function guardarTareas(){
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+function cargarTareas(){
+    const tareasGuardadas = localStorage.getItem("tareas");
+
+    if(tareasGuardadas){
+        tareas = JSON.parse(tareasGuardadas);
+    }
+}
